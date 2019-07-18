80e7740b80f998f45214f09627e23cc2ea199cf1
Natalya Gubina <n.gubina@webmikorn.ru>
2019-07-18 13:28:51 +0300

    no message

--- views/includes/site_head.php
+++
@@ -1,8 +0,0 @@
-  <head>
-    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Website title</title>
-    <link rel="stylesheet" href="/assets/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/font-awesome/css/fontawesome-all.min.css">
-  </head>
